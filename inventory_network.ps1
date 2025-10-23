688456ea474835889bbb8501a0af9d9d2a52d1b7
faraagon <simme.f@telia.com>
2025-10-23 20:05:55 +0200

    added section for recently modified

--- inventory_network.ps1
+++ inventory_network.ps1
@@ -20,4 +20,27 @@ foreach ($file in $allConfigs) {
     Write-Host "$($file.Name) - $([math]::Round($file.Length / 1KB, 2)) KB - ändrad: $($file.LastWriteTime)"
 }
 
+#Section for recently modified files
+Write-Host "2. Filer ändrade de senaste 7 dagarna"
+Write-Host "--------------------"
+
+#Go through network_configs and get files
+$recentFiles = Get-ChildItem -Path "network_configs" -Recurse -File |
+
+#Use Where-object to filter files with a LastWriteTime of 7 days or less
+Where-Object { $_.LastWriteTime -gt $weekAgo } |
+
+#Some files in assignment had 2025 as year for last edit
+#and I don't think that was on purpose so this line
+#should sort otu anything that is higher than the $now year, 2024
+Where-Object { $_.LastWriteTime.Year -le $now.Year } |
+
+#Sort from LastWriteTime, newest first
+Sort-Object LastWriteTime -Descending
+#Loop and print files names and date last edited
+foreach ($file in $recentFiles) {
+    Write-Host "$($file.Name) - ändrad: $($file.LastWriteTime)"
+}
+
+
 
